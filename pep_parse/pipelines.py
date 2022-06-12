233303c73d2f8f3396a945ee2c19fbb23b1d00fb
xrito <88393376+xrito@users.noreply.github.com>
2022-06-12 23:32:13 +0400

    Ver 1.0

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,13 +1,27 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import datetime as dt
+from collections import defaultdict
 
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+from pep_parse.constants import BASE_DIR, DATETIME_FORMAT
 
 
 class PepParsePipeline:
+    def open_spider(self, spider):
+        self.status_dict = defaultdict()
+
     def process_item(self, item, spider):
+        status = item['status']
+        self.status_dict[status] = self.status_dict.get(status, 0) + 1
         return item
+
+    def close_spider(self, spider):
+        results_dir = BASE_DIR / 'results'
+        results_dir.mkdir(exist_ok=True)
+        now = dt.datetime.now()
+        file_name = f'status_summary_{now.strftime(DATETIME_FORMAT)}.csv'
+        downloads_dir = results_dir / file_name
+        count_status_dict = sum(self.status_dict.values())
+        with open(downloads_dir, mode='w', encoding='utf-8') as f:
+            f.write('Статус,Количество\n')
+            for status, summ in self.status_dict.items():
+                f.write(f'{status},{summ}\n')
+            f.write(f'Total,{count_status_dict}\n')
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,10 +1,25 @@
+import re
+
 import scrapy
+from pep_parse.items import PepParseItem
 
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
     allowed_domains = ['peps.python.org']
-    start_urls = ['http://peps.python.org/']
+    start_urls = ['https://peps.python.org/']
 
     def parse(self, response):
-        pass
+        all_peps = response.css('a[href^="pep-"]')
+        for pep in all_peps:
+            yield response.follow(pep, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        number = re.findall(
+            r'\d+', response.css('h1.page-title::text').get().split(' – ')[0])
+        data = {
+            'number': number,
+            'name': response.css('h1.page-title::text').get(),
+            'status': response.css('dt:contains("Status") + dd::text').get()
+        }
+        yield PepParseItem(data)
